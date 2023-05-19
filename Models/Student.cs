a6fd095cd4981bf41e7f3a3c49211bed838f2689
Vaishnavi <87562957+iScreenager@users.noreply.github.com>
2023-05-19 12:03:15 +0530

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace RecapMvc.Models
+{
+    public class Student
+    {
+        public string Address { get; set; }
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+    }
+}
