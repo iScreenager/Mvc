dc8d17a44ae6955e4c57452633a5f82aeaaa5022
Vaishnavi <87562957+iScreenager@users.noreply.github.com>
2023-05-19 12:02:52 +0530

    Add files via upload

---
+++ Controllers/FirstExampleController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecapMvc.Models;
+
+namespace RecapMvc.Controllers
+{
+    public class FirstExampleController : Controller
+    {
+        public string TestMe()
+        {
+            return "Hello from MVC";
+        }
+
+        public Student DisplayStudent() 
+        {
+            return new Student
+            {
+                StudentId = 123, Address ="Chandigarh", Name ="Aakash"                
+            };
+        }
+
+        public ViewResult DisplayView()
+        {
+            var student = new Student
+            {
+                StudentId = 123,
+                Address = "Chandigarh",
+                Name = "Aakash"
+            };
+            return View(student);
+        }
+
+        public ViewResult AllStudents()
+        {
+            var data = new List<Student>
+            {
+                new Student{ StudentId = 111, Name = "Vaishnavi", Address ="Varanasi" },
+                new Student{ StudentId = 112, Name = "Aaakash", Address ="Chandigarh" },
+                new Student{ StudentId = 113, Name = "Anishika", Address ="Mathura" },
+                new Student{ StudentId = 114, Name="Shivani", Address ="Ludhiana"}
+            };
+            return View(data);
+        }
+    }
+}
